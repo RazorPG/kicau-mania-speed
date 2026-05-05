251cee9cfb0d095e35f4d401c795fa0283d01ef6
RazorPG <rafidhilmi0504@gmail.com>
2026-05-05 13:02:46 +0700

    memperbaiki design mobile

--- components/sections/Footer/Footer.tsx
+++ components/sections/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Footer() {
   return (
-    <footer className="flex items-center justify-center w-full p-4 text-white bg-transparent border-t border-gray-700 text-sm md:text-base">
+    <footer className="flex items-center justify-center w-full p-4 text-white bg-transparent border-t border-gray-700 text-sm md:text-base text-center">
       <p>
         &copy; {new Date().getFullYear()} Kicau Mania Speed. All rights reserved{" "}
         <a
--- components/views/Home/Home.tsx
+++ components/views/Home/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
   const { openSignIn } = useClerk()
   return (
     <main className="flex-1 flex flex-col items-center justify-center px-2 md:px-0 ">
-      <h1 className="text-4xl md:text-6xl font-bold text-white">
+      <h1 className="text-4xl md:text-6xl font-bold text-white text-center">
         Kicau Mania{" "}
         <span className="italic text-blue-600 mx-1 uppercase">Speed</span>
       </h1>
